f32308b6fd6413d9ebfd5d0d9dcd1b7bf83d6569
Amrakk <4mr4kk@gmail.com>
2023-04-12 20:17:12 +0700

    Temporarily remove composer[autoload, phpmailer] - add empty router, config file

---
+++ public/Pages/index.php
@@ -0,0 +1,16 @@
+<!-- WELCOME PAGE -->
+
+<?php
+    
+    // require_once '../src/Controller/Repo.php';
+
+    // use System\Controller\RepoController;
+    
+
+    // $repo = new RepoController();
+
+    // require_once './includes/header.php';
+    
+?>
+
+<h3>tmp</h3>
--- src/Routes/api.php
+++ src/Routes/api.php
@@ -6,3 +6,71 @@
     // receive the response from the controller
     // send the response back to the client
 ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!-- tmp -->
+
+
+<?php
+
+// Define a function for handling the home page
+function home_page() {
+    echo "Welcome to the home page!";
+}
+
+// Define a function for handling the about page
+function about_page() {
+    echo "This is the about page.";
+}
+
+// Define a function for handling a user profile page
+function user_profile($username) {
+    echo "This is the profile page for user $username.";
+}
+
+// Define an array of valid routes and their corresponding functions
+$routes = array(
+    '/' => 'home_page',
+    '/about' => 'about_page',
+    '/users/:username' => 'user_profile'
+);
+
+// Get the current request URL
+$request_url = $_SERVER['REQUEST_URI'];
+
+// Remove query string from the URL
+$request_url = strtok($request_url, '?');
+
+// Find the matching route for the request URL
+foreach ($routes as $route => $function) {
+    // Replace any URL parameters with a regex pattern
+    $route_pattern = preg_replace('/:[^\/]+/', '([^\/]+)', $route);
+
+    // Check if the request URL matches the route pattern
+    if (preg_match('#^' . $route_pattern . '$#', $request_url, $matches)) {
+        // Call the corresponding function with any URL parameters
+        array_shift($matches);
+        call_user_func_array($function, $matches);
+        exit();
+    }
+}
+
+// If no matching route was found, return a 404 error
+header("HTTP/1.0 404 Not Found");
+echo "Page not found.";
