335bef3d8b177c1c4d3c6e9e28e91ac5119ffdca
Amrakk <4mr4kk@gmail.com>
2023-04-17 23:30:15 +0700

    Push test 2

--- app/Models/Auth/Account.php
+++ app/Models/Auth/Account.php
@@ -4,8 +4,9 @@
     use App\Models\Shared\Database;
 
     class Account {
-        
-        
+        public $test = new Database();
+
+
         public function getAccountByUserName($username) {
             $conn = new Database();
             $data = $conn->selectQuery("SELECT * FROM accounts WHERE username = ?", [$username]);
